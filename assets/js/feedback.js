3a8731f8570c68ac907aeaa7598d9ff280f316a1
Robert Crooks <rcrooks@brightcove.com>
2020-05-26 14:46:38 -0400

    add files

---
+++ assets/js/feedback.js
@@ -0,0 +1,7 @@
+var fby = fby || [];
+fby.push(['showTab', {id: '15078', position: 'right', color: 'rgb(149, 192, 57)'}]);
+(function () {
+    var f = document.createElement('script'); f.type = 'text/javascript'; f.async = true;
+    f.src = '//cdn.feedbackify.com/f.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(f, s);
+})();
